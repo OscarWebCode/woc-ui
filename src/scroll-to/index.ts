d8f0cad2ed38baa89e163aa968b869dbebdfce48
Oscar Web <web.oscar.code@gmail.com>
2022-09-13 12:48:02 +0300

    Update Scroll To

--- src/scroll-to/index.ts
+++ src/scroll-to/index.ts
@@ -1,10 +1,21 @@
+export type WOCScrollToOptions = {
+  gap: number;
+};
 export class WOCScrollTo {
-  constructor(public gap: number) {}
-  scrollTo(element: HTMLElement): void {
-    if (!element) return;
-    window.scrollTo({
-      top: element.offsetTop - this.gap,
-      behavior: 'smooth',
-    });
+  private _gap: number;
+  private _behavior: ScrollBehavior = 'smooth';
+  constructor(options: WOCScrollToOptions) {
+    this._gap = options.gap || 0;
+  }
+  public scrollTo(element: HTMLElement): void {
+    if (!element) {
+      throw new Error('The element should be HTMLElement');
+    }
+    if (window && window.hasOwnProperty('scrollTo')) {
+      window.scrollTo({
+        top: element.offsetTop - this._gap,
+        behavior: this._behavior,
+      });
+    }
   }
 }
